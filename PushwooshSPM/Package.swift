7cfa5ee017f4330dc4f7c55cf2c8f0c6cab1a02b
André Kis <akiselev@pushwoosh.com>
2024-11-11 16:47:59 +0100

    added Pushwoosh dependency via local Package.swift

---
+++ PushwooshSPM/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "newdemo",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "newdemo",
+            targets: ["newdemo"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Pushwoosh/Pushwoosh-XCFramework.git", exact: "6.7.9"),
+    ],
+    targets: [
+        .target(
+            name: "newdemo",
+            dependencies: [
+                .product(name: "PushwooshFramework", package: "Pushwoosh-XCFramework")
+            ],
+            path: "Sources/newdemo"
+        )
+    ]
+)
